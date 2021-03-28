04cb1d0962d13b95a80c899ca19a427707622357
wr0n9man <rafigov2017@yandex.ru>
2021-03-28 11:28:06 +0400

    fixed:json-server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-  static: './build',
+  static: './.next',
 });
 
 const PORT = process.env.PORT || 4000;
